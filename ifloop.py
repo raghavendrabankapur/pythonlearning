838bd74cf9dfc9466eda2ee26334f14b116f1200
ananthr <raghavendra.ananthasetty@autodesk.com>
2016-11-29 14:56:28 +0800

    udpated the ifloop.py file

--- ifloop.py
+++ ifloop.py
@@ -1,2 +1,26 @@
-# if loop in pyton
-print("test the if loop")
+# printing range
+# range prints total number of value and not till that value
+for i in range(5):
+	print(i)
+
+print("printing specific range")	
+for i in range(5,10):
+	print(i)
+	
+print("printing negative value")	
+for i in range(-5,0):
+	print(i)
+
+print("printing with some proper increment. Below will print from 0-10 with difference of 3 between them as 0,3,6,9")
+for i in range(0,10,3):
+	print(i)
+	
+	
+x=["raghu",["padma","bhuvan","ruthu"],"autodesk","singapore"]
+for i in range(len(x)):
+	"""if len(i) > 1:
+		for t in range(len(x[i])):
+			print("printing inner index")
+			print(t,x[i,t])"""
+	#if:
+	print(i,x[i]+" with length "+len(x[i]))
